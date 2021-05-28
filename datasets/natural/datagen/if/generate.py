e1124d04f44409727f7be72fd508fcecc54db435
Rogerio Chaves <rogeriocfj@gmail.com>
2021-05-28 18:14:11 +0200

    Adds 'and' and 'or' conditions

--- datasets/natural/datagen/if/generate.py
+++ datasets/natural/datagen/if/generate.py
@@ -11,7 +11,7 @@ templates = {
   ">=": ["#A >= #B",
          "#A is greater or equal than #B"],
   "<": ["#A < #B",
-        "#A is simaller than #B"],
+        "#A is smaller than #B"],
   "<=": ["#A <= #B",
          "#A is smaller or equal than #B"],
   "!=": ["#A != #B",
@@ -20,17 +20,30 @@ templates = {
          "#A /= #B"]
 }
 
+composition_templates = {
+  "and": ["#0 and #1",
+          "#0 && #1"],
+  "or": ["#0 or #1",
+         "#0 || #1"],
+}
+
 condition_templates = [
   ("if #COND", "if #COND:"),
   ("if #COND", "if #COND:"),
+  ("if #COND", "if #COND:"),
+  ("#CASE_TRUE if #COND", "if #COND:\\n  #CASE_TRUE"),
   ("#CASE_TRUE if #COND", "if #COND:\\n  #CASE_TRUE"),
   ("#CASE_TRUE if #COND", "if #COND:\\n  #CASE_TRUE"),
   ("if not #COND", "if not #COND:"),
+  ("if not #COND", "if not #COND:"),
   ("#CASE_TRUE if #COND else #CASE_FALSE", "if #COND:\\n  #CASE_TRUE\\nelse:\\n  #CASE_FALSE"),
+  ("#CASE_TRUE if #COND else #CASE_FALSE", "if #COND:\\n  #CASE_TRUE\\nelse:\\n  #CASE_FALSE"),
+  ("unless #COND", "if not #COND:"),
   ("unless #COND", "if not #COND:"),
   ("else", "else:"),
   ("if not then", "else:")
 ]
+
 
 def generate_expression_sample():
   if choice(["assignment", "update"]) == "assignment":
@@ -38,23 +51,46 @@ def generate_expression_sample():
   else:
     return operations.generate_sample_update()
 
+
 def generate_value_or_operation():
   if choice(["value", "operation"]) == "value":
     return generate_value()
   else:
     return operations.generate_sample()
 
-inputs = []
-outputs = []
-for i in range(1000):
+
+def generate_comparison():
   comparison = choice(list(templates.keys()))
   template = choice(templates[comparison])
   (a, a_value) = generate_value_or_operation()
   (b, b_value) = generate_value_or_operation()
 
-  (condition_input, condition_output) = condition_templates[choice(len(condition_templates))]
   comparison_input = template.replace("#A", a).replace("#B", b)
   comparison_output = "%s %s %s" % (a_value, comparison, b_value)
+
+  if choice(["single", "nested"]) == "single":
+    return (comparison_input, comparison_output)
+  else:
+    composition = choice(list(composition_templates.keys()))
+    composition_template = choice(composition_templates[composition])
+
+    (nested_input, nested_output) = generate_comparison()
+
+    if choice(["parens", "no_parens"]) == "parens":
+      composition_input = composition_template.replace("#0", "(" + comparison_input + ")").replace("#1", "(" + nested_input + ")")
+      composition_output = "(%s) %s (%s)" % (comparison_output, composition, nested_output)
+    else:
+      composition_input = composition_template.replace("#0", comparison_input).replace("#1", nested_input)
+      composition_output = "%s %s %s" % (comparison_output, composition, nested_output)
+
+    return (composition_input, composition_output)
+
+
+inputs = []
+outputs = []
+for i in range(2000):
+  (comparison_input, comparison_output) = generate_comparison()
+  (condition_input, condition_output) = condition_templates[choice(len(condition_templates))]
 
   (case_true_input, case_true_output) = generate_expression_sample()
   (case_false_input, case_false_output) = generate_expression_sample()
