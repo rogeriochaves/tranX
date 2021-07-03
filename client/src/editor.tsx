fa39e139585023d75516748a188fd12df80a765f
Rogerio Chaves <rogeriocfj@gmail.com>
2021-07-03 19:39:10 +0200

    Refactor and improve the UI a great deal, no absolute positioning for the output anymore

--- client/src/editor.tsx
+++ client/src/editor.tsx
@@ -5,6 +5,71 @@ import axios from "axios";
 import type { State, Action } from "./state";
 import { Failure, Loading, RemoteData, Success } from "./utils/remoteData";
 
+export default function Editor(props: {
+  state: State;
+  dispatch: Dispatch<Action>;
+}) {
+  const windowDimensions = useWindowDimensions();
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [textAreaHeight, setTextAreaHeight] = useState("auto");
+  const [parentHeight, setParentHeight] = useState("auto");
+
+  const topbarHeight = 10;
+  const fontSize = 30;
+  const lineHeight = 2;
+
+  const getTextAreaHeight = () => textAreaRef.current!.scrollHeight;
+
+  const setCanvasHeight = () => {
+    setParentHeight(`${getTextAreaHeight()}px`);
+    setTextAreaHeight(`${getTextAreaHeight()}px`);
+  };
+
+  useEffect(setCanvasHeight, [props.state.input]);
+
+  useEffect(() => {
+    setTextAreaHeight("auto");
+    setTimeout(setCanvasHeight, 0);
+  }, [windowDimensions]);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const input = event.target.value;
+
+    setTextAreaHeight("auto");
+    props.dispatch({ type: "SET_INPUT", value: input });
+
+    runDebouncedParse(input, props.dispatch);
+  };
+
+  return (
+    <>
+      <div
+        className="topbar"
+        style={{ width: "100%", height: topbarHeight }}
+      ></div>
+      <Row style={{ maxWidth: 1200, margin: "0 auto" }}>
+        <Canvas
+          state={props.state}
+          dispatch={props.dispatch}
+          textAreaRef={textAreaRef}
+          textAreaHeight={textAreaHeight}
+          parentHeight={parentHeight}
+          onChangeHandler={onChangeHandler}
+          fontSize={fontSize}
+          lineHeight={lineHeight}
+          topbarHeight={topbarHeight}
+        />
+        <ParsedCode
+          state={props.state}
+          parentHeight={parentHeight}
+          fontSize={fontSize}
+          lineHeight={lineHeight}
+        />
+      </Row>
+    </>
+  );
+}
+
 function Canvas(props: {
   state: State;
   dispatch: Dispatch<Action>;
@@ -13,6 +78,7 @@ function Canvas(props: {
   textAreaHeight: string;
   fontSize: number;
   lineHeight: number;
+  topbarHeight: number;
   onChangeHandler: React.ChangeEventHandler<HTMLTextAreaElement>;
 }) {
   const runCodeButtonDisabled = Object.values(props.state.parsedLines).some(
@@ -38,36 +104,36 @@ function Canvas(props: {
     <div
       className="canvas"
       style={{
-        minHeight: props.parentHeight,
+        minHeight: `calc(100vh - ${props.topbarHeight}px)`,
         width: "100%",
         minWidth: 300,
         maxWidth: 800,
-        position: "relative",
       }}
     >
-      <textarea
-        className="canvas-textarea"
-        data-testid="canvas-textarea"
-        ref={props.textAreaRef}
-        rows={1}
-        style={{
-          width: "100%",
-          height: props.textAreaHeight,
-          padding: 30,
-          fontSize: props.fontSize,
-          lineHeight: props.lineHeight,
-        }}
-        onChange={props.onChangeHandler}
-        placeholder="type some code..."
-        value={props.state.input}
-      />
       <div
         style={{
-          position: "absolute",
-          left: "30px",
-          top: 50 + props.state.input.split("\n").length * 60 + "px",
+          minHeight: props.parentHeight,
+          position: "relative",
         }}
       >
+        <textarea
+          className="canvas-textarea"
+          data-testid="canvas-textarea"
+          ref={props.textAreaRef}
+          rows={1}
+          style={{
+            width: "100%",
+            height: props.textAreaHeight,
+            padding: 30,
+            fontSize: props.fontSize,
+            lineHeight: props.lineHeight,
+          }}
+          onChange={props.onChangeHandler}
+          placeholder="type some code..."
+          value={props.state.input}
+        />
+      </div>
+      <div style={{ padding: "0 30px" }}>
         <button
           data-testid="run-code"
           onClick={runCode}
@@ -75,23 +141,10 @@ function Canvas(props: {
         >
           Run Code
         </button>
-        <div data-testid="output">{props.state.output}</div>
+        <Output state={props.state} />
       </div>
     </div>
   );
-}
-
-function parsedLineAsText(parsedLine: RemoteData<string>): string {
-  switch (parsedLine.state) {
-    case "NOT_ASKED":
-      return "";
-    case "LOADING":
-      return "*";
-    case "FAILURE":
-      return parsedLine.error;
-    case "SUCCESS":
-      return parsedLine.data;
-  }
 }
 
 function ParsedCode(props: {
@@ -126,6 +179,53 @@ function ParsedCode(props: {
           </div>
         )
       )}
+    </div>
+  );
+}
+
+function parsedLineAsText(parsedLine: RemoteData<string>): string {
+  switch (parsedLine.state) {
+    case "NOT_ASKED":
+      return "";
+    case "LOADING":
+      return "*";
+    case "FAILURE":
+      return parsedLine.error;
+    case "SUCCESS":
+      return parsedLine.data;
+  }
+}
+
+function Output(props: { state: State }) {
+  return (
+    <div style={{ paddingTop: "30px" }}>
+      {props.state.output && (
+        <>
+          <div
+            className="output-title"
+            style={{
+              position: "absolute",
+              margin: "-1px 0 0 10px",
+              padding: "0 5px",
+            }}
+          >
+            output:
+          </div>
+          <hr />
+        </>
+      )}
+      <div
+        className="output"
+        data-testid="output"
+        style={{ paddingTop: "20px" }}
+      >
+        {(props.state.output.toString() || "").split("\n").map((line, i) => (
+          <React.Fragment key={i}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
@@ -182,72 +282,4 @@ function runDebouncedParse(input: string, dispatch: Dispatch<Action>) {
 
     parseCalls[lineNumber] = { line: inputLine, timeout };
   }
-}
-
-export default function Editor(props: {
-  state: State;
-  dispatch: Dispatch<Action>;
-}) {
-  const windowDimensions = useWindowDimensions();
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const [textAreaHeight, setTextAreaHeight] = useState("auto");
-  const [parentHeight, setParentHeight] = useState("auto");
-
-  const topbarHeight = 10;
-  const fontSize = 30;
-  const lineHeight = 2;
-
-  const getTextAreaHeight = () =>
-    Math.max(
-      textAreaRef.current!.scrollHeight,
-      windowDimensions.height - topbarHeight - 2
-    );
-
-  const setCanvasHeight = () => {
-    setParentHeight(`${getTextAreaHeight()}px`);
-    setTextAreaHeight(`${getTextAreaHeight()}px`);
-  };
-
-  useEffect(setCanvasHeight, [props.state.input]);
-
-  useEffect(() => {
-    setTextAreaHeight("auto");
-    setTimeout(setCanvasHeight, 0);
-  }, [windowDimensions]);
-
-  const onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const input = event.target.value;
-
-    setTextAreaHeight("auto");
-    props.dispatch({ type: "SET_INPUT", value: input });
-
-    runDebouncedParse(input, props.dispatch);
-  };
-
-  return (
-    <>
-      <div
-        className="topbar"
-        style={{ width: "100%", height: topbarHeight }}
-      ></div>
-      <Row style={{ maxWidth: 1200, margin: "0 auto" }}>
-        <Canvas
-          state={props.state}
-          dispatch={props.dispatch}
-          textAreaRef={textAreaRef}
-          textAreaHeight={textAreaHeight}
-          parentHeight={parentHeight}
-          onChangeHandler={onChangeHandler}
-          fontSize={fontSize}
-          lineHeight={lineHeight}
-        />
-        <ParsedCode
-          state={props.state}
-          parentHeight={parentHeight}
-          fontSize={fontSize}
-          lineHeight={lineHeight}
-        />
-      </Row>
-    </>
-  );
 }
